84b4affceb805e1369fb5dabc854b43f6fddbf89
Fabian-Gorka <fabian-gorka@lambdastudents.com>
2021-03-16 19:23:07 -0500

    mvp

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -8,7 +8,7 @@ const AdditionalFeatures = props => {
       {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature key={item.id} feature={item}/>
           ))}
         </ol>
       ) : (
@@ -17,5 +17,4 @@ const AdditionalFeatures = props => {
     </div>
   );
 };
-
 export default AdditionalFeatures;
